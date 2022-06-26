dc4cca79a633781b0a02ac56084bcde186fefec2
changwoon2 <changwoon4@naver.com>
2022-06-26 16:28:52 +0900

    save

--- main.js
+++ main.js
@@ -1,101 +1,116 @@
-// 유저가 값을 입력한다
-// + 버튼을 클릭하면, 핳일이 추가된다
-// delete 버튼을 누르면 할일이 삭제된다
-// check버튼을 누르면 할일이 끝나면서 밑줄이 간다
-// 진행중 끝남 탭을 누르면, 언더바가 이동한다
-// 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만
-// 전체탭을 누르면 다시 전체하이템으로 돌아옴
-
-let taskInput = document.getElementById("task-input");
-let addButton = document.getElementById("add-button");
-let tabs = document.querySelectorAll(".task-tabs div");
+let userInput = document.querySelector(".task-input");
+let addButton = document.querySelector(".button-add");
+let tabs = document.querySelectorAll(".tab-type div");
+let underLine = document.getElementById("tab-underline");
 let taskList = [];
-let filterList = [];
-let mode = "all";
-addButton.addEventListener("click", addTask);
+let selectedMenu = "tab-all";
+let filteredList = [];
 
-for (let i = 1; i < tabs.length; i++) {
+addButton.addEventListener("mousedown", addTask);
+userInput.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    addTask(event);
+  }
+});
+for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", function (event) {
     filter(event);
   });
 }
 
 function addTask() {
+  let taskValue = userInput.value;
   let task = {
-    id: randomIDGenerate(),
-    taskContent: taskInput.value,
+    content: taskValue,
     isComplete: false,
+    id: randomIDGenerator(),
   };
+
   taskList.push(task);
-  console.log(taskList);
+  userInput.value = "";
   render();
 }
 
 function render() {
-  let list = [];
-  if (mode == "all") {
-    list == taskList;
-  } else if (mode == "ongoing") {
-    list = filterList;
+  let result = "";
+  list = [];
+  if (selectedMenu === "tab-all") {
+    list = taskList;
+  } else {
+    list = filteredList;
   }
 
-  let resultHTML = "";
   for (let i = 0; i < list.length; i++) {
-    if (list[i].isComplete == true) {
-      resultHTML += `<div class="task">
-      <div class="task-done">${list[i].taskContent}</div>
-      <div>
-      <button onclick="toggleComplete('${list[i].id}')">Check</button>
-      <button onclick="deleteTask('${list[i].id}')">Delete</button>
-      </div>
-      </div>`;
+    if (list[i].isComplete) {
+      result += `<div class="task task-done" id="${list[i].id}">
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
     } else {
-      resultHTML += `<div class="task">
-    <div>${list[i].taskContent}</div>
-    <div>
-    <button onclick="toggleComplete('${list[i].id}')">Check</button>
-    <button onclick="deleteTask('${list[i].id}')">Delete</button>
-    </div>
-    </div>`;
+      result += `<div class="task" id="${list[i].id}" >
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fa fa-check"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
     }
   }
-  document.getElementById("task-board").innerHTML = resultHTML;
+
+  document.getElementById("task-board").innerHTML = result;
 }
 
-function toggleComplete(id) {
+function toggleDone(id) {
   for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].id == id) {
+    if (taskList[i].id === id) {
       taskList[i].isComplete = !taskList[i].isComplete;
       break;
     }
   }
-  render();
-  console.log(taskList);
+  filter();
 }
 
 function deleteTask(id) {
   for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].id == id) {
+    if (taskList[i].id === id) {
       taskList.splice(i, 1);
-      break;
+    }
+  }
+
+  filter();
+}
+function filter(e) {
+  if (e) {
+    selectedMenu = e.target.id;
+    underLine.style.width = e.target.offsetWidth + "px";
+    underLine.style.left = e.target.offsetLeft + "px";
+    underLine.style.top =
+      e.target.offsetTop + (e.target.offsetHeight - 4) + "px";
+  }
+
+  filteredList = [];
+  if (selectedMenu === "tab-not-done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filteredList.push(taskList[i]);
+      }
+    }
+  } else if (selectedMenu === "tab-done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        filteredList.push(taskList[i]);
+      }
     }
   }
   render();
 }
 
-function filter(event) {
-  mode = event.target.id;
-  let filterList = [];
-  if (mode == "all") {
-    render();
-  } else if (mode == "ongoing") {
-    for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete == false) filterList.push(taskList[i]);
-    }
-  }
-  render();
-}
-console.log(filterList);
-function randomIDGenerate() {
+function randomIDGenerator() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
   return "_" + Math.random().toString(36).substr(2, 9);
 }
